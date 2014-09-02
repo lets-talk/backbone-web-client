3a76b10bb5cb31a1af8344a1cfb1ed9e652c1930
Felipe Peña <felipe@penya.cl>
2014-09-02 13:45:27 -0400

    Better styling for chat list

--- js/views/chat_list_view.js
+++ js/views/chat_list_view.js
@@ -3,6 +3,9 @@ window.ChatListView = Backbone.View.extend({
     model: Chat,
 
     render: function() {
-    	this.$el.append('<a href="#chat/' + this.model.get('id') + '">' + this.model.get('issue') + '</a>');
+    	var lastMessage = this.model.get('last_message');
+    	var lastMessageContent = lastMessage.content;
+    	var lastMessageTime = lastMessage.created_at;
+    	this.$el.append('<img width="70" height="70" src="' + this.model.get('client').avatar + ' "/><div class="message"><a href="#chat/' + this.model.get('id') + '">' + this.model.get('issue') + '</a><p class="last_message">' + lastMessageContent + '</p><p class="last_message_time"></p></div>');
     }
 });
