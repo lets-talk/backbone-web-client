cfaaeba08e21695ed1357614d6433ac9701f062e
Felipe Peña <felipe@penya.cl>
2014-09-15 14:09:14 -0300

    Let's Talk logo added to sample page

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
 	<title>Let's Talk</title>
 </head>
 <body>
-
+	<div style="margin: 0 auto; width: 100%; text-align: center;">
+		<img style="margin-top:100px" src="img/logo.png">
+	</div>
 </body>
 </html>
