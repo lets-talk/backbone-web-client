0c31bccb7a34d10757128fd17d2d45b45bd78344
Felipe Peña <felipe@penya.cl>
2014-09-17 13:32:29 -0300

    Fix within gh-pages

--- views/widget/init.js.php
+++ views/widget/init.js.php
@@ -8,17 +8,6 @@
 {
     /*!
      * contentloaded.js
-     *
-     * Author: Diego Perini (diego.perini at gmail.com)
-     * Summary: cross-browser wrapper for DOMContentLoaded
-     * Updated: 20101020
-     * License: MIT
-     * Version: 1.2
-     *
-     * URL:
-     * http://javascript.nwbox.com/ContentLoaded/
-     * http://javascript.nwbox.com/ContentLoaded/MIT-LICENSE
-     *
      */
     function contentLoaded(c,h){var b=false,g=true,j=c.document,i=j.documentElement,m=j.addEventListener?"addEventListener":"attachEvent",k=j.addEventListener?"removeEventListener":"detachEvent",a=j.addEventListener?"":"on",l=function(n){if(n.type=="readystatechange"&&j.readyState!="complete"){return}(n.type=="load"?c:j)[k](a+n.type,l,false);if(!b&&(b=true)){h.call(c,n.type||n)}},f=function(){try{i.doScroll("left")}catch(n){setTimeout(f,50);return}l("poll")};if(j.readyState=="complete"){h.call(c,"lazy")}else{if(j.createEventObject&&i.doScroll){try{g=!c.frameElement}catch(d){}if(g){f()}}j[m](a+"DOMContentLoaded",l,false);j[m](a+"readystatechange",l,false);c[m](a+"load",l,false)}};
     // Utils
@@ -35,7 +24,7 @@
         var MOBILE_BUTTON_SIZE = 55;
         
         iframe.id               = 'customer-chat-iframe';
-        iframe.src              = '<?php echo $app->path("Widget:iframeContent") ?>&domain=' + document.domain;
+        iframe.src              = 'http://letstalk.asheschile.com<?php echo $app->path("Widget:iframeContent") ?>&domain=' + document.domain;
         iframe.border           = 0;
         iframe.marginwidth      = 0;
         iframe.marginWidth      = 0;
